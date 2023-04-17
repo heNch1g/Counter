234fb8de70e4bfcdab5cec59305d1e9eb89388e4
Alexey Volovikov <hench1g@gmail.com>
2023-04-18 01:18:06 +0900

    Первая версия сложной задачи - работает все кроме логики текст вью

--- Counter/ViewController.swift
+++ Counter/ViewController.swift
@@ -7,23 +7,47 @@
 
 import UIKit
 
+
+
+
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-        addButton.setTitle("Увеличить счетчик", for: .normal)
         counterNumber.text = "Значение счётчика: \n 0"
+        textView.text = "История изменений:"
     }
     
     var count: Int = 0
+     
+    //строковая переменная с датой и временем  https://www.donnywals.com/formatting-dates-in-swift-using-date-formatstyle-on-ios-15/
+    
+    let formatted = Date().formatted(date: .complete, time: .standard)
+    
+    
     @IBOutlet weak var counterNumber: UILabel!
-    @IBOutlet weak var addButton: UIButton!
-    @IBAction func countAddButton(_ sender: Any) {
-        count += 1
-        counterNumber.text = "Значение счётчика: \n \(count)"
+    @IBOutlet weak var textView: UITextView!
+    
+    @IBAction func minusButton(_ sender: Any) {
+        if count == 0 { } else { count -= 1 }
+        changeCount()
     }
     
+    @IBAction func resetButton(_ sender: Any) {
+        count = 0
+        changeCount()
+    }
     
+    @IBAction func plusButton(_ sender: Any) {
+        count += 1
+        changeCount()
+    }
+    
+    // временно стоит изменения текствью на дату - работает!
+    func changeCount() {
+        counterNumber.text = "Значение счётчика: \n \(count)"
+        textView.text = formatted
+    }
 }
 
