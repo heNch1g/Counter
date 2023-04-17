1f0897b9ad5aafda278e4482300b4c4a30f29b34
Alexey Volovikov <hench1g@gmail.com>
2023-04-18 00:47:49 +0900

    Легкий вариант задачи

--- Counter/ViewController.swift
+++ Counter/ViewController.swift
@@ -12,8 +12,18 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        addButton.setTitle("Увеличить счетчик", for: .normal)
+        counterNumber.text = "Значение счётчика: \n 0"
     }
-
-
+    
+    var count: Int = 0
+    @IBOutlet weak var counterNumber: UILabel!
+    @IBOutlet weak var addButton: UIButton!
+    @IBAction func countAddButton(_ sender: Any) {
+        count += 1
+        counterNumber.text = "Значение счётчика: \n \(count)"
+    }
+    
+    
 }
 
