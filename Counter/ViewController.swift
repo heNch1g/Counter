7dd33fd5a44759ce84ae36f14579a584eeaadb09
Alexey Volovikov <hench1g@gmail.com>
2023-04-18 13:41:44 +0900

    Усложненная задача - все работает

--- Counter/ViewController.swift
+++ Counter/ViewController.swift
@@ -7,47 +7,59 @@
 
 import UIKit
 
-
-
+//Экстенш для текствью чтоб скроллить вниз
+extension UITextView {
+    func simple_scrollToBottom() {
+        let textCount: Int = text.count
+        guard textCount >= 1 else { return }
+        scrollRangeToVisible(NSRange(location: textCount - 1, length: 1))
+    }
+}
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
-        counterNumber.text = "Значение счётчика: \n 0"
+
+        counterNumber.text = "Значение счётчика: \n \n 0"
         textView.text = "История изменений:"
     }
-    
+   
+// count - значения счетчика, actionName - строка с описанием действия
     var count: Int = 0
-     
-    //строковая переменная с датой и временем  https://www.donnywals.com/formatting-dates-in-swift-using-date-formatstyle-on-ios-15/
-    
-    let formatted = Date().formatted(date: .complete, time: .standard)
-    
+    var actionName: String = ""
+// formatted - строка с датой и временем, хз как сделать ее в 24часовом формате
+    let formatted = Date().formatted()
     
     @IBOutlet weak var counterNumber: UILabel!
     @IBOutlet weak var textView: UITextView!
-    
     @IBAction func minusButton(_ sender: Any) {
-        if count == 0 { } else { count -= 1 }
-        changeCount()
+        if count == 0 {
+            actionName = "Попытка уменьшить значение счётчика ниже 0"
+            changeCount()
+        } else {
+            count -= 1
+            actionName = "Значение изменено на -1"
+            changeCount()
+        }
     }
     
     @IBAction func resetButton(_ sender: Any) {
         count = 0
+        actionName = "Значение сброшено"
         changeCount()
     }
     
     @IBAction func plusButton(_ sender: Any) {
         count += 1
+        actionName = "Значение изменено на +1"
         changeCount()
     }
     
-    // временно стоит изменения текствью на дату - работает!
     func changeCount() {
-        counterNumber.text = "Значение счётчика: \n \(count)"
-        textView.text = formatted
+        counterNumber.text = "Значение счётчика: \n \n \(count)"
+        textView.text += "\n" + formatted + "   " + actionName
+        textView.simple_scrollToBottom()
     }
 }
 
